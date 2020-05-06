6a3bdd6a16cc3d90494be68889cde6b5dad93f5f
rainerceribelli <rainer.rusctiguel2013@gmail.com>
2020-05-06 19:42:42 -0300

    deus

---
+++ site aps/javascript/script.js
@@ -0,0 +1,38 @@
+const debounce = function(func, wait, immediate) {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+  const windowTop = window.pageYOffset + (window.innerHeight * 0.95);
+  target.forEach((element) => {
+    console.log('i')
+    if(windowTop > element.offsetTop) {
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+  })
+}
+
+animeScroll();
+
+const handleScroll = debounce(animeScroll, 200);
+
+if(target.length) {
+  window.addEventListener('scroll', handleScroll);
+}
+
